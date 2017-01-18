a6faed2d3c9be0081829a7e70ba7d472dd562e3c
hahahahahanananana <hanahanamocomoco@softbank.ne.jp>
2017-01-18 20:17:55 +0900

    Initial Commit

---
+++ photo math/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  photo math
+//
+//  Created by hahahahahanananana on 2017/01/18.
+//  Copyright © 2017年 片岡. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
